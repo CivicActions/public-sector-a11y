c207c2eab59959d6d48ee105fe82e34335d1442b
Bentley Hensel <bentley.hensel@civicactions.com>
2023-01-24 18:44:09 -0500

    MOVING RUST TO NEW REPO
    Having Rust in sub-directory has overly complicated this repo. Moving the Rust containers out to enable streamlined development.
    
    New home for CivicRust is [CivicActions/public-sector-a11y_connect](https://github.com/CivicActions/public-sector-a11y_connect)

--- rust-checker/crawl.rs
+++
@@ -1,7 +0,0 @@
-/*  Domain Scanner
- * Sends POST Request to A11yWatch API to initiate website Crawl
- * Captures results and sends to BigQuery
- * Written by: @theBoatyMcBoatFace
- * On: Jan 24, 2023
- */
-
